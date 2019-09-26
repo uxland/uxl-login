5c50bcc8ada323744cc3d5c429822d41b3f7862c
EstherGaldeano <egaldeano@uxland.es>
2019-09-26 18:46:57 +0200

    user/password check

--- src/components/uxl-login/template.ts
+++ src/components/uxl-login/template.ts
@@ -1,7 +1,9 @@
 
 import { html } from "lit-element";
+import { classMap } from "lit-html/directives/class-map";
+import { UxlLogin } from './uxl-login';
 
-export const template = (props) => html`
+export const template = (props:UxlLogin) => html`
 
 
 <h2>Login</h2>
@@ -10,14 +12,18 @@ export const template = (props) => html`
   <div class="login">
     <div class="inputs">
       <div class="inputs__username">
-        <input class="username" autofocus autocapitalize="none" type= "email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
+        <input class="username"
+         autofocus
+         autocapitalize="none" 
+         type= "email" 
+         pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
       </div>
       <div class="inputs__password">
         <input class="password" type="password"/>
       </div>       
     </div>
   </div>
-  <button class= "btn-acceder">location?</button>
+  <button class="btn-acceder" .disabled="${!props.canSubmit}">Entrar</button>
 </div>
 
 `;
--- src/components/uxl-login/uxl-login.ts
+++ src/components/uxl-login/uxl-login.ts
@@ -2,10 +2,14 @@
 import { css, customElement, html, LitElement, property, query, unsafeCSS } from "lit-element";
 import styles from "./styles.scss";
 import { template } from "./template";
+import { listen, isNotNullNeitherEmpty } from "@uxland/uxl-utilities";
 
 @customElement("uxl-login")
 
 export class UxlLogin extends LitElement {
+
+  @property()
+  public canSubmit: boolean = false;
 
   @query(".username")
   public userNameInput: any;
@@ -16,8 +20,46 @@ export class UxlLogin extends LitElement {
   @query(".btn-acceder")
   public btnAcceder: any;
 
+  //decorator listen (event listener, pasas acción y elemento al que se le aplica)
+  @listen("click", ".btn-acceder")
+  onClick(){
+    console.log("soy un botón");
+  }
 
-   static get styles() {
+  @listen("keyup", ".password"||".username")
+  public onKeyUpPassword(){
+    this.userCanSubmit();
+  }
+
+  //user can login if username and password are typed
+
+  //ponemos tanto el valor del elemento como el elemento, por si no existiera
+  public userCanSubmit(){
+    this.canSubmit = this.userNameInput 
+                    && isNotNullNeitherEmpty(this.userNameInput.value) 
+                    && this.passwordInput 
+                    && isNotNullNeitherEmpty(this.passwordInput.value);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  static get styles() {
     return css`
       ${unsafeCSS(styles)}
     `;
@@ -25,9 +67,6 @@ export class UxlLogin extends LitElement {
 
   public render() {
 
-    warning();
-    
-  
     return html`
       ${template(this)}
     `;
@@ -36,8 +75,3 @@ export class UxlLogin extends LitElement {
 
 
 }
-
-
-function warning(): void {
-  alert("This is my warning message");
-  }
